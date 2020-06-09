70f3e3772f1c7151145e6a92e5dffd928a99526c
Arvind Raghavan <arvind.raghavan@robinhood.com>
2020-06-09 12:31:09 -0500

    whitespace

--- install.sh
+++ install.sh
@@ -25,15 +25,15 @@ fi
 # Install vim packages
 if [ ! -d ~/.vim/pack/plugins/start/vimwiki ]; then
     echo "Installing vimwiki..." ;
-    git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki ; 
+    git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki ;
 fi
 
 if [ ! -d ~/.vim/pack/plugins/start/surround ]; then
     echo "Installing surround..." ;
-    git clone https://tpope.io/vim/surround.git ~/.vim/pack/plugins/start/surround ; 
+    git clone https://tpope.io/vim/surround.git ~/.vim/pack/plugins/start/surround ;
 fi
 
 if [ ! -d ~/.vim/pack/plugins/start/commentary ]; then
     echo "Installing commentary..." ;
-    git clone https://tpope.io/vim/commentary.git ~/.vim/pack/plugins/start/commentary ; 
+    git clone https://tpope.io/vim/commentary.git ~/.vim/pack/plugins/start/commentary ;
 fi
