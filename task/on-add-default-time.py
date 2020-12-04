e2e01a2ef6e998eb93a96832831bc0e06dd20c9a
Arvind Raghavan <raghavan.arvind@gmail.com>
2020-12-04 13:02:58 -0600

    Add task hooks for fix default time

---
+++ task/on-add-default-time.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import sys
+import re
+import json
+from datetime import time, datetime
+from dateutil import parser
+
+DEFAULT_TIME = time(22,0,0)
+
+def is_local_midnight(dt):
+    return dt.astimezone().time() == time(0,0,0)
+
+def set_default_time(timestamp):
+    return timestamp.astimezone().replace(
+        hour=DEFAULT_TIME.hour,
+        minute=DEFAULT_TIME.minute,
+        second=DEFAULT_TIME.second,
+    ).isoformat()
+
+def hook_default_time(task, due):
+        task['due'] = set_default_time(due)
+        print("Default due time has been set.")
+
+task = json.loads(sys.stdin.readline())
+
+if task['due']:
+    due = parser.parse(task["due"])
+    if is_local_midnight(due):
+        hook_default_time(task, due)
+
+print(json.dumps(task))
